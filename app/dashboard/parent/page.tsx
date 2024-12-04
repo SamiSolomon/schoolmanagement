4d25432fcbe3e4da1ae3495fd604a77755d2287c
Samuel Solomon <samuelsolomon452@gmail.com>
2024-12-04 08:50:21 +0300

    starting the Next js school management project

---
+++ app/dashboard/parent/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const parent = () => {
+  return (
+    <div>
+      parent
+    </div>
+  )
+}
+
+export default parent
---
+++ app/dashboard/student/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const student= () => {
+  return (
+    <div>
+      student
+    </div>
+  )
+}
+
+export default student
